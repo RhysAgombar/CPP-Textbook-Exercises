0ea3e0eef2fbef26be7a1c5ae286d22076556ce1
RhysAgombar <r.agombar@gmail.com>
2024-08-09 17:33:25 +0200

    Finished Array Drill

--- Ch18/Ch18.cpp
+++ Ch18/Ch18.cpp
@@ -3,7 +3,30 @@
 
 #include <iostream>
 
+using namespace std;
+
+int ga[10] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+
+void f(int in[], int len) {
+    int* la = new int[len];
+    int testarr[10];
+
+    
+    copy(in, in + 10, testarr);
+    for (int i = 0; i < 10; i++) {
+        cout << testarr[i] << " ";
+    }
+    cout << std::endl;
+
+    copy(in, in + len, la);
+    for (int i = 0; i < 10; i++) {
+        cout << la[i] << " ";
+    }
+    cout << std::endl;
+    delete[] la;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    f(ga, 10);
 }
